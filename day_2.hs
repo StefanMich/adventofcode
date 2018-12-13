3fb77b6a8f26bb9e9ef01755f7929d63299af3ba
Stefan Micheelsen <stufkan@gmail.com>
2018-12-13 22:52:10 +0100

    working map on list of strings

--- day_2.hs
+++ day_2.hs
@@ -5,4 +5,9 @@ counter [] m = m
 counter [x] m = Data.Map.insertWith (+) x 1 m
 counter (x:xs) m = counter xs (Data.Map.insertWith (+) x 1 m)
 
+main = do
+  i <- readFile "day_2_testinput"
+
+  let count = Prelude.fmap (\x -> counter x Data.Map.empty) $ lines i
+  putStrLn $ show count
 
